aef67e16855c4f31a6092e22d24a134dd85ba15f
Cathy Yu <cathyyu2018@gmail.com>
2022-04-29 21:43:07 -0700

    Add id to user ddb schema

--- server/models/UserDDB.js
+++ server/models/UserDDB.js
@@ -2,6 +2,7 @@ const dynamooseclient = require('./dynamooseclient');
 
 // create a new model class
 const userDDBSchema = new dynamooseclient.Schema({
+  "id": String,
   "googleId": String,
   "credits": { "type": Number, "default": 0 }
 });
