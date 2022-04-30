f44eef990b8c22f5fb59ad35b49b107fa56179dc
Cathy Yu <cathyyu2018@gmail.com>
2022-04-29 21:22:28 -0700

    Migrate user to ddb

--- server/services/passport.js
+++ server/services/passport.js
@@ -15,9 +15,15 @@ passport.serializeUser((user, done) => {
 
 // take out the user id from session
 passport.deserializeUser((id, done) => {
-  User.findById(id).then(user => {
-    done(null, user);
-  });
+  const esistingUserFromDDB = await userDDBModel.get({"id": id});
+  if(esistingUserFromDDB) {
+    console.log("line 20 user: ", esistingUserFromDDB)
+    done(null, esistingUserFromDDB);
+  }
+
+  // User.findById(id).then(user => {
+  //   done(null, user);
+  // });
 });
 
 
@@ -25,7 +31,8 @@ passport.deserializeUser((id, done) => {
 // and if succeed, execute the async function to load or create user
 passport.use(
   new LocalStrategy(async function verify(username, password, done) {
-    const existingUser = await User.findOne({ googleId : "103805923715121791269" });
+    // const existingUser = await User.findOne({ googleId : "103805923715121791269" });
+    const esistingUserFromDDB = await userDDBModel.scan("googleId").eq("103805923715121791269").exec();
     console.log("mongodb is finished line 29");
     console.log("ddb processing starting line 20");
 
@@ -34,14 +41,15 @@ passport.use(
 
     // console.log("ddb processing done and get user googleid: ", esistingUserFromDDB[0].googleId);
 
-    console.log("existing user's id type: ", typeof(existingUser.id));
-    console.log("existing user's id in mongodb: ", existingUser.id);
-    const existingUserMongo = await User.findById(existingUser.id);
-    console.log("existing user's id type in mongodbbbb: ", typeof(mongoose.Types.ObjectId(existingUser.id)));
+    // console.log("existing user's id type: ", typeof(existingUser.id));
+    // console.log("existing user's id in mongodb: ", existingUser.id);
+    // const existingUserMongo = await User.findById(existingUser.id);
+    // console.log("existing user's id type in mongodbbbb: ", typeof(mongoose.Types.ObjectId(existingUser.id)));
 
-    if(existingUser) {
+    if(esistingUserFromDDB[0]) {
+      console.log("line 52 user: ", esistingUserFromDDB[0])
       // we already have a record with the given profile ID
-      done(null, existingUser);
+      done(null, esistingUserFromDDB[0]);
       console.log("done");
     }
   })
